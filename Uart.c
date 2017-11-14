7fdae4c4c59ab03f7d0980874f79753f2e5cc891
Tyler Matthews <trm84@zips.uakron.edu>
2017-11-13 21:31:26 -0500

    Minor, misc updates

--- Uart.c
+++ Uart.c
@@ -15,32 +15,34 @@ void UART_Init(){
     U1BRG = temp;
     
     // U1MODE Register settings
-    U1MODEbits.UARTEN = 1;
-    U1MODEbits.USIDL = 0;
-    U1MODEbits.IREN = 0;
-    U1MODEbits.RTSMD = 0;
-    U1MODEbits.UEN = 0b00;
-    U1MODEbits.WAKE = 0;
+    U1MODEbits.UARTEN = 1; //Enable UART1
+    U1MODEbits.USIDL = 0; //Continue during idle
+    U1MODEbits.IREN = 0; //IrDA encoder/decoder disable
+    U1MODEbits.RTSMD = 0; //U1RTS in flow control mode
+    U1MODEbits.UEN = 0b00; //U1TX and U1RX are enabled and used. UxCTS and UxRTS/BCLK pins controlled by port latches
+    U1MODEbits.WAKE = 0; //No wake-up enabled
     U1MODEbits.LPBACK = 0;
     U1MODEbits.ABAUD = 0;
     U1MODEbits.URXINV = 0;
-    U1MODEbits.BRGH = 0;
-    U1MODEbits.PDSEL = 0b00;
+    U1MODEbits.BRGH = 0; //16x baud clock
+    U1MODEbits.PDSEL = 0b00; //8bit data - no parity
     U1MODEbits.STSEL = 0;
     
-    U1STAbits.URXISEL = 0b00;
-    U1STAbits.UTXINV = 0;
+    U1STAbits.URXISEL = 0b00;   //Interrupt when a character is transferred to the Transmit Shift register (this implies there is at least one character open in the transmit buffer)
+    U1STAbits.UTXINV = 0;       //0 = UxTX Idle state is ?1?
     U1STAbits.UTXBRK = 0;
-    U1STAbits.UTXEN = 1;
+    U1STAbits.UTXEN = 1;    //Transmit enable bit
     U1STAbits.URXISEL = 0b00;
-    U1STAbits.ADDEN = 0;
-    U1STAbits.OERR = 0;
+    U1STAbits.ADDEN = 0; //Address detect bit
+    U1STAbits.OERR = 0;  //Overrun Error Status Bit
     
     
     
     
 }
 
+
+//Will need to send up for one byte at a time for now
 void UARTSend(Speed *data[]){
     U1TXREG[0] = data[0]->speedH;
     U1TXREG[1] = data[0]->speedL;
